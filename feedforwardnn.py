1015088269434d44ee528e9ae89e14bfbc6e7312
sooraj-srini <cs20b075@smail.iitm.ac.in>
2023-03-11 12:30:41 +0530

    Working momentum and RMSprop

--- feedforwardnn.py
+++ feedforwardnn.py
@@ -2,31 +2,38 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-class SGD:
-    def __init__(self, weights, bias, learning_rate: float):
-        self.learning_rate = learning_rate
-        self.weights = weights
-        self.bias = bias
-    
 
-    def step(self, dW: list[np.ndarray], db: list[np.ndarray]):
-        for i in range(1, len(self.weights)):
-            self.weights[i] -= self.learning_rate * dW[i]
-            self.bias[i] -= self.learning_rate * db[i]
+class SGD:
+    def __init__(self, parameters, learning_rate: float):
+        self.learning_rate = learning_rate
+        self.parameters = parameters
+
+    def zero_grad(self):
+        pass
+
+    def step(self):
+        for i in range(1, len(self.parameters.weights)):
+            self.parameters.weights[i] -= self.learning_rate * self.parameters.dW[i]
+            self.parameters.bias[i] -= self.learning_rate * self.parameters.db[i]
+
 
 class Momentum:
-    def __init__(self, weights, bias, learning_rate: float, momentum: float):
+    def __init__(self, params, learning_rate: float, momentum: float):
         self.learning_rate = learning_rate
         self.momentum = momentum
         self.vW = None
         self.vb = None
-        self.weights = weights
-        self.bias = bias
+        self.parameters = params
+        self.weights = params.weights
+        self.bias = params.bias
+
     def zero_grad(self):
         self.vW = None
-        self.vb = None 
+        self.vb = None
 
-    def step(self, dW: list[np.ndarray], db: list[np.ndarray]):
+    def step(self):
+        dW = self.parameters.dW
+        db = self.parameters.db
         if self.vW is None:
             self.vW = [None] * len(self.weights)
             self.vb = [None] * len(self.weights)
@@ -40,17 +47,104 @@ class Momentum:
             self.weights[i] -= self.learning_rate*self.vW[i]
             self.bias[i] -= self.learning_rate*self.vb[i]
 
+class RMSProp:
+    def __init__(self, params, learning_rate: float, decay_rate: float, epsilon: float = 1e-8):
+        self.learning_rate = learning_rate
+        self.decay_rate = decay_rate
+        self.sW = None
+        self.sb = None
+        self.params = params
+        self.weights = params.weights
+        self.bias = params.bias
+        self.epsilon = epsilon
+    def zero_grad(self):
+        self.sW = None
+        self.sb = None
+    def step(self):
+        dW = self.params.dW
+        db = self.params.db
+
+        if self.sW is None:
+            self.sW = [None] * len(self.weights)
+            self.sb = [None] * len(self.weights)
+            for i in range(1, len(self.weights)):
+                self.sW[i] = np.zeros(self.weights[i].shape)
+                self.sb[i] = np.zeros(self.bias[i].shape)
+
+        for i in range(1, len(self.weights)):
+            self.sW[i] = self.decay_rate * self.sW[i] + (1 - self.decay_rate) * dW[i]**2
+            self.sb[i] = self.decay_rate * self.sb[i] + (1 - self.decay_rate) * db[i]**2
+            self.weights[i] -= self.learning_rate * dW[i] / (np.sqrt(self.sW[i]) + self.epsilon)
+            self.bias[i] -= self.learning_rate * db[i] / (np.sqrt(self.sb[i]) + self.epsilon)
+
+class Adam:
+    def __init__(self, weights, bias, learning_rate: float, decay_rate: float, momentum: float, epsilon: float = 1e-8):
+        self.learning_rate = learning_rate
+        self.decay_rate = decay_rate
+        self.momentum = momentum
+        self.sW = None
+        self.sb = None
+        self.vW = None
+        self.vb = None
+        self.weights = weights
+        self.bias = bias
+        self.epsilon = epsilon
+        self.t = 0
+    def zero_grad(self):
+        self.sW = None
+        self.sb = None
+        self.vW = None
+        self.vb = None
+    def step(self, dW: list[np.ndarray], db: list[np.ndarray]):
+        if self.sW is None:
+            self.sW = [None] * len(self.weights)
+            self.sb = [None] * len(self.weights)
+            self.vW = [None] * len(self.weights)
+            self.vb = [None] * len(self.weights)
+            for i in range(1, len(self.weights)):
+                self.sW[i] = np.zeros(self.weights[i].shape)
+                self.sb[i] = np.zeros(self.bias[i].shape)
+                self.vW[i] = np.zeros(self.weights[i].shape)
+                self.vb[i] = np.zeros(self.bias[i].shape)
+
+        self.t += 1
+        for i in range(1, len(self.weights)):
+            self.vW[i] = self.momentum * self.vW[i] + (1 - self.momentum) * dW[i]
+            self.vb[i] = self.momentum * self.vb[i] + (1 - self.momentum) * db[i]
+            self.sW[i] = self.decay_rate * self.sW[i] + (1 - self.decay_rate) * dW[i]**2
+            self.sb[i] = self.decay_rate * self.sb[i] + (1 - self.decay_rate) * db[i]**2
+            self.weights[i] -= self.learning_rate * self.vW[i] / (np.sqrt(self.sW[i]) + self.epsilon)
+            self.bias[i] -= self.learning_rate * self.vb[i] / (np.sqrt(self.sb[i]) + self.epsilon)
+
+class NAG(Momentum):
+    # Implement Nesterov accelerated gradient descent 
+    def step(self, dW: list[np.ndarray], db: list[np.ndarray]):
+        if self.vW is None:
+            self.vW = [None] * len(self.weights)
+            self.vb = [None] * len(self.weights)
+            for i in range(1, len(self.weights)):
+                self.vW[i] = np.zeros(self.weights[i].shape)
+                self.vb[i] = np.zeros(self.bias[i].shape)
+
+        for i in range(1, len(self.weights)):
+            self.vW[i] = self.momentum * self.vW[i] + dW[i]
+            self.vb[i] = self.momentum * self.vb[i] + db[i]
+            self.weights[i] -= self.learning_rate*(self.momentum*self.vW[i] + dW[i])
+            self.bias[i] -= self.learning_rate*(self.momentum*self.vb[i] + db[i])
+
 class FeedforwardNN:
-
-
+    class Parameters:
+        def __init__(self):
+            pass
     def __init__(self, input_size: int, output_size: int, hidden_size_layers: list[int], activation_function: str):
+        self.parameters = self.Parameters()
         self.input_size = input_size
         self.hidden_size_layers = hidden_size_layers
         self.output_size = output_size
-        self.activation, self.derivate_activation = self.activation_function(
+        self.parameters.activation_func, self.parameters.derivate_activation = self.activation_function(
             activation_function)
-        self.weights: list[np.ndarray] = [None]
-        self.bias: list[np.ndarray] = [None]
+        self.parameters.weights: list[np.ndarray] = [None]
+        self.parameters.bias: list[np.ndarray] = [None]
         self.layers = len(hidden_size_layers) + 1
 
         # Initalise weights
@@ -63,20 +157,22 @@ class FeedforwardNN:
         self.weights.insert(index, np.random.randn(
             self.hidden_size_layers[index-1], self.hidden_size_layers[index]))
         # Add bias for new layer
-        self.bias.insert(index, np.random.randn(1,self.hidden_size_layers[index]))
+        self.bias.insert(index, np.random.randn(
+            1, self.hidden_size_layers[index]))
 
     def init_weights(self):
-        self.weights.append(np.random.randn(
+        self.parameters.weights.append(np.random.randn(
             self.input_size, self.hidden_size_layers[0]))
-        self.bias.append(np.random.randn(1, self.hidden_size_layers[0]))
+        self.parameters.bias.append(np.random.randn(1, self.hidden_size_layers[0]))
         for i in range(len(self.hidden_size_layers) - 1):
-            self.weights.append(np.random.randn(
+            self.parameters.weights.append(np.random.randn(
                 self.hidden_size_layers[i], self.hidden_size_layers[i+1]))
             # Add bias
-            self.bias.append(np.random.randn(1, self.hidden_size_layers[i+1]))
-        self.weights.append(np.random.randn(
+            self.parameters.bias.append(np.random.randn(1, self.hidden_size_layers[i+1]))
+        
+        self.parameters.weights.append(np.random.randn(
             self.hidden_size_layers[-1], self.output_size))
-        self.bias.append(np.random.randn(1,self.output_size))
+        self.parameters.bias.append(np.random.randn(1, self.output_size))
 
         # Add bias
 
@@ -106,29 +202,40 @@ class FeedforwardNN:
             return tanh, derivative_tanh
         else:
             raise ValueError("Activation function not supported")
-    def use_activation(self, func: str):
-        self.activation, self.derivate_activation = self.activation_function(func)
 
-    def forward(self, X: np.ndarray):
+    def use_activation(self, func: str):
+        self.parameters.activation_func, self.parameters.derivate_activation = self.activation_function(
+            func)
+
+    def forward(self):
         # Forward pass
+        X = self.parameters.x_row
         pre_activation = [X]
         activation = [X]
-        for i in range(1, len(self.weights)-1):
-            X = np.dot(X, self.weights[i])
-            X = X + self.bias[i]
+        for i in range(1, len(self.parameters.weights)-1):
+            X = np.dot(X, self.parameters.weights[i])
+            X = X + self.parameters.bias[i]
             pre_activation.append(X)
-            X = self.activation(X)
+            X = self.parameters.activation_func(X)
             activation.append(X)
 
         def softmax(X: np.ndarray):
             return np.exp(X - np.expand_dims(np.max(X, axis=1), axis=1))/np.expand_dims(np.sum(np.exp(X - np.expand_dims(np.max(X, axis=1), axis=1)), axis=1), axis=1)
 
-        X = np.dot(X, self.weights[-1])
+        X = np.dot(X, self.parameters.weights[-1])
         pre_activation.append(X)
         X = softmax(X)
-        return X, pre_activation, activation
 
-    def backpropagate(self, pre_activation, activation, y_train, y_pred):
+        self.parameters.y_pred  = X
+        self.parameters.pre_activation = pre_activation
+        self.parameters.activation = activation
+
+    def backward(self):
+        pre_activation = self.parameters.pre_activation
+        activation = self.parameters.activation
+        y_train = self.parameters.y_row
+        y_pred = self.parameters.y_pred
+
         dW = [None]*(self.layers+1)
         db = [None]*(self.layers+1)
         dlda = [None]*(self.layers+1)
@@ -146,37 +253,41 @@ class FeedforwardNN:
 
             if layer == 1:
                 break
-            dldh[layer-1] = dlda[layer]@self.weights[layer].T
-            derivate_activation = self.derivate_activation(
+            dldh[layer-1] = dlda[layer]@self.parameters.weights[layer].T
+            derivate_activation = self.parameters.derivate_activation(
                 pre_activation[layer-1])
             dlda[layer-1] = dldh[layer-1] * derivate_activation
 
-
-        return dW, db
+        self.parameters.dW = dW
+        self.parameters.db = db
 
     def fit(self, X_train, y_train, learning_rate, max_iterations, batch):
-        opt = SGD(self.weights, self.bias, learning_rate)
-        # opt = Momentum(self.weights, self.bias, learning_rate, 0.9)
+        # opt = SGD(self.get_params(), learning_rate)
+        # opt = Momentum(self.get_params(), learning_rate, 0.9)
+        opt = RMSProp(self.get_params(), learning_rate, 0.9)
         for iter in range(max_iterations):
             opt.zero_grad()
             print("Iteration: ", iter)
-            y_pred, pre_activation, activation = self.forward(X_train)
             for index in range(0, len(X_train), batch):
-                x_row = X_train[index:index + batch]
-                y_row = y_train[index:index + batch]
-                y_pred, pre_activation, activation = self.forward(x_row)
-                dW, db = self.backpropagate(
-                    pre_activation, activation, y_row, y_pred)
-                opt.step(dW, db)
-                # for i in range(1,len(self.weights)):
-                    # self.weights[i] -= learning_rate*dW[i]
-                    # self.bias[i] -= learning_rate*db[i]
+                self.parameters.x_row = X_train[index:index + batch]
+                self.parameters.y_row = y_train[index:index + batch]
+                self.forward()
+                self.backward()
+                opt.step()
             y_iter_pred = self.predict(X_train)
             print("Loss: ", self.cross_entropy_loss(y_train, y_iter_pred))
 
     def predict(self, X_test):
-        y_pred, _, _ = self.forward(X_test)
-        return y_pred
+        self.parameters.x_row = X_test
+        self.forward()
+        return self.parameters.y_pred
         # return np.argmax(y_pred, axis=1)
+
+    def get_params(self):
+        return self.parameters
+
+
     def cross_entropy_loss(self, y_train, y_pred):
         return -np.sum(y_train*np.log(y_pred))/len(y_train)
+    def accuracy(self, y_train, y_pred):
+        return (y_train == y_pred).sum() / (y_train.shape[1]*y_train.shape[0])
--- train.py
+++ train.py
@@ -1,4 +1,30 @@
 import argparse
+import feedforwardnn
+import wandb
+import numpy as np
+from keras.datasets import fashion_mnist
+wandb.login()
+
+
+
+def train(config):
+    nn = config.nn
+    opt = config.opt
+    x_train = config.x_train
+    y_train = config.y_train
+    opt.zero_grad()
+    for index in range(0, len(x_train), config.batch):
+        x_row = x_train[index:index+config.batch]
+        y_row = y_train[index:index+config.batch]
+        y_pred, pre_activation, activation = nn.forward(x_row)
+        dW, db = nn.backpropagate(pre_activation, activation, y_row, y_pred)
+        opt.step(dW, db)
+    y_iter_pred = nn.predict(x_train)
+    loss = nn.cross_entropy_loss(y_train, y_iter_pred)
+    accuracy = nn.accuracy(y_train, y_iter_pred)
+    return loss, accuracy
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -9,7 +35,7 @@ if __name__ == "__main__":
     parser.add_argument("-b", "--batch_size", help="Batch size", type=int, default=4)
     parser.add_argument("-l", "--loss", help="Loss function", type=str, default="cross_entroy", choices=["mean_squared_error", "cross_entropy"])
     parser.add_argument("-o", "--optimizer", help="Optimizer", type=str, default="sgd", choices=["sgd", "momentum", "nag", "rmsprop", "adam", "nadam"])
-    parser.add_argument("--lr", "--learning-rate", help="Value of the learning rate", type=float, default=0.1)
+    parser.add_argument("-lr", "--learning_rate", help="Value of the learning rate", type=float, default=0.1)
     parser.add_argument("-m", "--momentum", help="Value of the momentum", type=float, default=0.5)
     parser.add_argument("-beta", "--beta", help="Value of beta", type=float, default=0.5)
     parser.add_argument("-beta1", "--beta1", help="Value of beta1", type=float, default=0.5)
@@ -20,4 +46,24 @@ if __name__ == "__main__":
     parser.add_argument("-nhl", "--num_hidden_layers", help="Number of hidden layers", type=int, default=1)
     parser.add_argument("-sz", "--hidden_size", help="Size of hidden layers", type=int, default=4)
     parser.add_argument("-a", "--activation", help="Activation function", type=str, default="sigmoid", choices=["identity", "sigmoid", "ReLU", "tanh"])
-    parser.parse_args()
+    args = parser.parse_args()
+    
+    # start processing the data
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    x_train = x_train.reshape(x_train.shape[0], -1)
+    def one_hot(X: np.ndarray):
+        return np.eye(10)[X]
+    y_train = one_hot(y_train)
+
+    sweep_config = {
+        "method": "random",
+        "metric": {"goal": "minimize", "name": "loss"},
+        "parameters": {
+            "hidden_layer_size": {"values": [32, 64, 128]},
+            "num_hidden_layers": {"values": [3, 4, 5]}, 
+            "learning_rate": {"values": [0.001, 0.0001]}
+        }
+    }
+
+    sweep_id = wandb.sweep(sweep=sweep_config, project='CS6910 Assignment 1')
+    wandb.agent(sweep_id=sweep_id, function=wandb_sweep, count=10)
